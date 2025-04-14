d78860bd2240f9bcc12e898ed17d2a5009de3f08
Jason <wjosunar@gmail.com>
2025-04-14 23:58:36 +0800

    testing

--- planner.js
+++ planner.js
@@ -6,7 +6,18 @@ function showView(viewId, btn) {
     const navitems = document.querySelectorAll('.nav-item');
     navitems.forEach(navitem => navitem.classList.remove('selected'));
     navitems[btn].classList.add('selected');
-  }
+}
+
+function showForm(formId, btn) {
+    const forms = document.querySelectorAll('form');
+    forms.forEach(f => f.classList.remove('active'));
+    document.getElementById(formId).classList.add('active');
+
+    const formselections = document.querySelectorAll('.form-menu-item');
+    formselections.forEach(s => s.classList.remove('selected'));
+    formselections[btn].classList.add('selected');
+}
+
 
 const form = document.getElementById('form-add-cp');
 
