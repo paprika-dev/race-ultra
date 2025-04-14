b440c202602ae8b70f7069ae5c71953bd90acff6
Jason <wjosunar@gmail.com>
2025-04-14 16:53:12 +0800

    testing

--- planner.js
+++ planner.js
@@ -1,6 +1,11 @@
-function showView(viewId) {
+function showView(viewId, btn) {
     const views = document.querySelectorAll('.view');
     views.forEach(view => view.classList.remove('active'));
     document.getElementById(viewId).classList.add('active');
-    console.log('hiii')
+
+    const navitems = document.querySelectorAll('.nav-item');
+    navitems.forEach(navitem => navitem.classList.remove('selected'));
+    navitems[btn].classList.add('selected');
   }
+
+document.addEventListener("touchstart", function(){}, true);
